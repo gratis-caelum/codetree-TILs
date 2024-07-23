5b648a4b9329f404bdf05b1c9623f2f1389af6a4
Chan <127709404+gratis-caelum@users.noreply.github.com>
2024-07-23 16:28:40 +0900

    Title: 함수를 이용한 윤년 판별, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240723/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
+++ 240723/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
@@ -24,22 +24,21 @@ public class Main {
             System.out.println("숫자를 잘못 입력하였습니다. (1 <= year <= 2021)");
             System.exit(0);
         }
-
-        if (year % 4 == 0)
-        {
-            if (year % 100 == 0)
-            {
-                if (year % 400 == 0)
-                {
-                    return true; // 400으로 나누어 떨어지는 해는 윤년
-                } else {
-                    return false; // 예외적으로 100으로 나누어 떨어지되 400으로 나누어 떨어지지 않는 해는 평년
-                }
-            } else {
-                return true; // 4로 나누어 떨어지되 100으로 나누어 떨어지지 않는 해는 윤년
-            }
-        } else {
-            return false; // 4로 나누어 떨어지지 않는 해는 평년
+        // 4로 나누어 떨어지지 않는 경우 평년
+        if (year % 4 != 0) {
+            return false;
         }
-    }
+
+        // 4로 나누어 떨어지는 경우 + 100으로 나누어 떨어지지 않는 경우는 윤년
+        if (year % 100 != 0) {
+            return true;
+        }
+
+        // 4로 나누어 떨어지는 경우 + 100으로 나누어 떨어지는 경우 + 400으로 나누어 떨어지지 않는 경우는 평년
+        if (year % 400 != 0)
+        {
+            return false;
+        }
+        // 4로 나누어 떨어지지만, 예외적인 사항은 전부 확인했으므로 윤년
+        return true;
 }
