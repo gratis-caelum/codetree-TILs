48afceaad997555f6da29817473f742b995c4335
Chan <127709404+gratis-caelum@users.noreply.github.com>
2024-08-15 03:35:09 +0900

    Title: 아름다운 수열 2, Time: 162ms, Memory: 10MB, Status: Passed - CodeTree

--- 240815/아름다운 수열 2/beautiful-sequence-2.java
+++ 240815/아름다운 수열 2/beautiful-sequence-2.java
@@ -8,77 +8,48 @@ public class Main {
         int N = scanner.nextInt();
         int M = scanner.nextInt();
         scanner.nextLine(); // 개행 문자 소비
-        
+
         // 수열 A 입력
         int[] A = new int[N];
         for (int i = 0; i < N; i++) {
             A[i] = scanner.nextInt();
         }
-        
+
         // 수열 B 입력
         int[] B = new int[M];
         for (int i = 0; i < M; i++) {
             B[i] = scanner.nextInt();
         }
-        
-        // 수열 B의 모든 순열 생성
-        List<List<Integer>> permutations = new ArrayList<>();
-        generatePermutations(B, 0, permutations);
-        
+
+        // 수열 B의 원소 빈도수 계산
+        Map<Integer, Integer> bFrequency = new HashMap<>();
+        for (int num : B) {
+            bFrequency.put(num, bFrequency.getOrDefault(num, 0) + 1);
+        }
+
         // 수열 A에서 길이가 M인 모든 연속 부분 수열 생성 및 비교
         int count = 0;
         for (int i = 0; i <= N - M; i++) {
             int[] subArray = Arrays.copyOfRange(A, i, i + M);
-            if (isBeautifulSequence(subArray, permutations)) {
+            if (isBeautifulSequence(subArray, bFrequency)) {
                 count++;
             }
         }
-        
+
         // 결과 출력
         System.out.println(count);
 
         scanner.close();
     }
 
-    // 수열 B의 모든 순열을 생성하는 함수
-    private static void generatePermutations(int[] B, int start, List<List<Integer>> permutations) {
-        if (start == B.length) {
-            List<Integer> permutation = new ArrayList<>();
-            for (int num : B) {
-                permutation.add(num);
-            }
-            permutations.add(permutation);
-            return;
-        }
-        
-        for (int i = start; i < B.length; i++) {
-            swap(B, start, i);
-            generatePermutations(B, start + 1, permutations);
-            swap(B, start, i);
-        }
-    }
-
-    // 배열의 두 요소를 교환하는 함수
-    private static void swap(int[] B, int i, int j) {
-        int temp = B[i];
-        B[i] = B[j];
-        B[j] = temp;
-    }
-
     // 부분 수열이 아름다운 수열인지 확인하는 함수
-    private static boolean isBeautifulSequence(int[] subArray, List<List<Integer>> permutations) {
-        for (List<Integer> permutation : permutations) {
-            boolean match = true;
-            for (int i = 0; i < subArray.length; i++) {
-                if (subArray[i] != permutation.get(i)) {
-                    match = false;
-                    break;
-                }
-            }
-            if (match) {
-                return true;
-            }
+    private static boolean isBeautifulSequence(int[] subArray, Map<Integer, Integer> bFrequency) {
+        Map<Integer, Integer> subArrayFrequency = new HashMap<>();
+        for (int num : subArray) {
+            subArrayFrequency.put(num, subArrayFrequency.getOrDefault(num, 0) + 1);
         }
-        return false;
+
+        // 두 빈도수 맵이 같은지 비교
+        return subArrayFrequency.equals(bFrequency);
     }
 }
