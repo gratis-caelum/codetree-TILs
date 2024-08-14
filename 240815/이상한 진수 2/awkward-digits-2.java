6ca35539f057d39450b9c40f5ee13dd8fb2aee08
Chan <127709404+gratis-caelum@users.noreply.github.com>
2024-08-15 03:11:39 +0900

    Title: 이상한 진수 2, Time: 84ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240815/이상한 진수 2/awkward-digits-2.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static int soln(String a) {
+        // 2진수로 변환
+        int[] binaryA = new int[a.length()];
+        for (int i = 0; i < a.length(); i++) {
+            binaryA[i] = a.charAt(i) - '0';
+        }
+
+        // 0인 자리를 1로 바꾸어 만들 수 있는 가장 큰 수 찾기
+        int maxNum = 0;
+        for (int i = 0; i < binaryA.length; i++) {
+            if (binaryA[i] == 0) {
+                binaryA[i] = 1;
+                int num = convertToDecimal(binaryA);
+                maxNum = Math.max(maxNum, num);
+                binaryA[i] = 0;
+            }
+        }
+
+        return maxNum;
+    }
+
+    private static int convertToDecimal(int[] binaryArray) {
+        int decimal = 0;
+        for (int i = 0; i < binaryArray.length; i++) {
+            decimal = decimal * 2 + binaryArray[i];
+        }
+        return decimal;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(soln("1010")); // 14
+    }
+}
