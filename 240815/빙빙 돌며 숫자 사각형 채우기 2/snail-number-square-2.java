6fe0e3563aa1c073e11838c3f51a10270ab02eb3
Chan <127709404+gratis-caelum@users.noreply.github.com>
2024-08-15 01:14:35 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240815/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.java
+++ 240815/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.java
@@ -1,47 +1,46 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int[] dx = {1, 0, -1, 0}; // 동 - 남 - 서 - 북
-    public static int[] dy = {0, -1, 0, 1}; // 동 - 남 - 서 - 북
     public static int n, m;
+    public static int[][] answer;
+    public static int[] dx = {0, 1, 0, -1}; // 북 - 동 - 남 - 서 기준
+    public static int[] dy = {1, 0, -1, 0}; // 북 - 동 - 남 - 서 기준
     public static final int MAX = 100;
-
     public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
         Scanner input = new Scanner(System.in);
 
-        // n * m 격자 입력
-        n = input.nextInt();
-        m = input.nextInt();
+        n = input.nextInt(); // n행
+        m = input.nextInt(); // m열
 
-        int[][] arr = new int[MAX + 1][MAX + 1];
+        answer = new int[n][m];
 
-
-        int dirNum = 0; // 남 : 0, 서 : 1, 북 : 2, 동 : 3
         int x = 0; int y = 0; // 초기 위치 설정
-        arr[0][0] = 1;
-        for (int i = 2; i < n * m; i++) {
-            // 위치 이동 (남 - 동 - 서 - 북)
+        answer[x][y] = 1; // 1부터 시작
+        int dirNum = 0; // 오른쪽 : 0, 아래 : 1, 왼쪽 : 2, 위 : 3
+
+        for (int i = 2; i <= n * m; i++) {
             int nx = x + dx[dirNum];
             int ny = y + dy[dirNum];
 
-            // 더 이상 나아갈 수 없으면 반시계방향으로 90도 회전
-            if (!inRange(nx, ny) || arr[nx][ny] != 0) {
-                dirNum = (dirNum + 3) % 4;
+             // 더 이상 나아갈 수 없다면 시계방향으로 90도 회전
+            if (!inRange(nx, ny) || answer[nx][ny] != 0) {
+                dirNum = (dirNum + 1) % 4; // 0 : 동쪽, 1 : 아래, 2 : 왼쪽, 3 : 위쪽
                 if (dirNum < 0) {
                     dirNum += 4;
                 }
             }
 
-            // 하나씩 이동할 때마다 값 저장
+            // 하나씩 이동할 때마다 값을 저장
             x += dx[dirNum];
             y += dy[dirNum];
-            arr[x][y] = i;
+            answer[x][y] = i;
         }
 
         // 출력
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                System.out.print(arr[i][j] + " ");
+                System.out.print(answer[i][j] + " ");
             }
             System.out.println();
         }
